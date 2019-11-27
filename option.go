13661516e4365b24ae2f18a727b2d0fd9d15dcc4
yangjun <290968698@qq.com>
2019-11-27 17:11:02 +0800

    add RecvDataSlicePolicy and some doc

--- option.go
+++ option.go
@@ -3,10 +3,20 @@ package yudp
 const mtu = 9001
 
 type YudpOption struct {
-	Addr         string
-	Port         int
-	ReusePort    bool
-	RecvBufSize  int
-	SendBufSize  int
+	//listen interface,default is empty,listen on all interface
+	Addr string
+	Port int
+	//in linux this can be true, other os is not supported
+	ReusePort bool
+	//udp socket recv buf size,if not set, use os default
+	RecvBufSize int
+	//udp socket send buf size,if not set, use os default
+	SendBufSize int
+	//in linux this is used by recvmmsg(default:64), other os is not used
 	UdpBatchSize int
+
+	//0: new buffer every read,udpHandler no need additional processing for data parameter
+	// in high pps programm, this will cause high gc
+	//1: use yudp.pool, user should call PutDataSlice2Pool after processing
+	RecvDataSlicePolicy int
 }
