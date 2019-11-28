2bd56b5e3267c99dc1b65ace86bc8fb633ec0d6c
yangjun <290968698@qq.com>
2019-11-28 17:13:04 +0800

    add some option to echo

--- example/echo/echo.go
+++ example/echo/echo.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net/http"
 
 	"github.com/yangjuncode/yudp"
 )
 
+import _ "net/http/pprof"
+
+var port int
+var policy int
+
 func main() {
+	flag.IntVar(&port, "port", 10000, "udp listen port")
+	flag.IntVar(&policy, "policy", 0, "data slice new policy")
+
+	flag.Parse()
+
+	go func() {
+		http.ListenAndServe(":8000", nil)
+	}()
+
 	opt := yudp.YudpOption{
-		Addr:         "",
-		Port:         10000,
-		ReusePort:    false,
-		RecvBufSize:  1 * 1024 * 1024,
-		SendBufSize:  1 * 1024 * 1024,
-		UdpBatchSize: 64,
+		Addr:                "",
+		Port:                port,
+		ReusePort:           false,
+		RecvBufSize:         1 * 1024 * 1024,
+		SendBufSize:         1 * 1024 * 1024,
+		UdpBatchSize:        64,
+		RecvDataSlicePolicy: policy,
 	}
 
 	udpCon, err := yudp.NewListener(opt)
@@ -22,5 +39,9 @@ func main() {
 	}
 	udpCon.Listen(func(data []byte, addr yudp.YudpAddr) {
 		udpCon.WriteTo(data, &addr)
+
+		if policy == 1 {
+			yudp.PutDataSlice2Pool(data)
+		}
 	})
 }
